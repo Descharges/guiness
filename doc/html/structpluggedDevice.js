9b3113ee2d810d44c5804ce5a5e41dfa6cbdba87
Paul <paul.caillier@utbm.fr>
2024-02-21 00:39:42 -0500

    Move documentation

--- doc/html/structpluggedDevice.js
+++
@@ -1,5 +0,0 @@
-var structpluggedDevice =
-[
-    [ "address", "structpluggedDevice.html#a444cb1b9a6d22c279bc404a48d8d6114", null ],
-    [ "device", "structpluggedDevice.html#ae403a23800de58ba22107e1db8575f31", null ]
-];

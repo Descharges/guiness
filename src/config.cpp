cba1b21df2defdce719e7cd290d7be9a1d6b2a6a
Paul <paul.caillier@utbm.fr>
2024-05-26 17:16:41 -0400

    Remove ConfigFactory

--- src/config.cpp
+++ src/config.cpp
@@ -1,63 +1,80 @@
 #include "config.hpp"
-#include "logger.hpp"
+
 #include <sstream>
 
-LoggerProxy logConf = Logger::newLoggerProxy("CONFIG");
+#include "logger.hpp"
+
+IdLogger logConf = Logger::newIdLogger("CONFIG");
 
 Config::Config(
     std::unordered_map<std::string, std::string> const &defaultConfig) {
-  this->configMap = defaultConfig;
+    this->configMap = defaultConfig;
+}
+
+Config *Config::defaultConfig() {
+    std::unordered_map<std::string, std::string> defaultConf = {
+        {"FPS", "60"}, {"BREAK_MODE", "NONE"},  // {NONE, FRAME, STEP}
+    };
+
+    return new Config(defaultConf);
+}
+
+Config *Config::configFromFile(std::string const &path) {
+    throw "Not implemented";
+}
+
+Config *Config::configFromArgs(int argc, char *argv[]) {
+    throw "Not implemented";
 }
 
 Config::~Config() {}
 
 bool Config::exists(std::string const &id) {
-  auto it = configMap.find(id);
+    auto it = configMap.find(id);
 
-  if (it == this->configMap.end()) {
-    return false;
-  };
+    if (it == this->configMap.end()) {
+        return false;
+    };
 
-  return true;
+    return true;
 }
 
 void Config::set(std::string const &id, std::string const &value) {
-  if (exists(id)) {
-    this->configMap[id] = value;
-    return;
-  }
+    if (exists(id)) {
+        this->configMap[id] = value;
+        return;
+    }
 
-  logConf.getStringStream()
-      << "Attempt to set unexisting config member : " << id;
-  logConf.flushSsLog(error);
+    logConf.getStringStream()
+        << "Attempt to set unexisting config member : " << id;
+    logConf.flushSsLog(error);
 }
 
 std::string Config::get(std::string const &id) {
+    if (exists(id)) {
+        return this->configMap[id];
+    }
 
-  if (exists(id)) {
-    return this->configMap[id];
-  }
-
-  logConf.getStringStream()
-      << "Attempt to get unexisting config member : " << id;
-  logConf.flushSsLog(error);
-  return "";
+    logConf.getStringStream()
+        << "Attempt to get unexisting config member : " << id;
+    logConf.flushSsLog(error);
+    return "";
 }
 
 bool Config::getAsBool(std::string const &id) {
-  throw "Config::getAsBool is not implemented yet.";
+    throw "Config::getAsBool is not implemented yet.";
 }
 
 unsigned int Config::getAsUInt(std::string const &id) {
-  throw "Config::getAsUInt is not implemented yet.";
+    throw "Config::getAsUInt is not implemented yet.";
 }
 
-void Config::logConfig()
-{
-  logConf << "\n\n" << "Configuration" << "\n-------------------";
-  for (const auto &i : configMap){
-    logConf << "\n" << i.first << "-->" << i.second;
-  }
-  logConf << "\n";
-  logConf.flushSsLog();
+void Config::logConfig() {
+    logConf << "\n\n"
+            << "Configuration" << "\n-------------------";
+    for (const auto &i : configMap) {
+        logConf << "\n" << i.first << "-->" << i.second;
+    }
+    logConf << "\n";
+    logConf.flushSsLog();
 }
--- src/config.hpp
+++ src/config.hpp
@@ -5,71 +5,86 @@
 #include <unordered_map>
 
 /**
- * @brief Stroes the configuration of the application.
+ * @brief Stores the configuration of the application.
  *
  * Acts as a wrapper around a map with key and values as string, but with
- * options to cast and with checks to forbid the addition of new memmbers
+ * options to cast and with checks to forbid the addition of new members
  * after object creation.
  */
 class Config {
-public:
-  /**
-   * @brief Construct a new Config object
-   *
-   */
-  Config(std::unordered_map<std::string, std::string> const &defaultConfig);
+   public:
+    /**
+     * @brief Construct a new Config object
+     *
+     */
+    Config(std::unordered_map<std::string, std::string> const &defaultConfig);
 
-  /**
-   * @brief Destroy the Config object
-   *
-   */
-  ~Config();
+    /**
+     * @brief Returns a new Config with default values
+     */
+    static Config *defaultConfig();
 
-  /**
-   * @brief Checks if an id is a config member.
-   *
-   * @param id The id to check
-   * @return true The id is a member of the configuration
-   * @return false The id is not a member of the configuration
-   */
-  bool exists(std::string const &id);
+    /**
+     *
+     */
+    static Config *configFromFile(std::string const &path);
 
-  /**
-   * @brief Sets the value of a configuration member.
-   *
-   * @param id The id of the member.
-   * @param value The value to set.
-   */
-  void set(std::string const &id, std::string const &value);
+    /**
+     *
+     */
+    static Config *configFromArgs(int argc, char *argv[]);
 
-  /**
-   * @brief Gets the value of a configuration member.
-   *
-   * @param id The id of the member.
-   * @return std::string The value of the member. Empty string if the member is
-   * not found.
-   */
-  std::string get(std::string const &id);
+    /**
+     * @brief Destroy the Config object
+     *
+     */
+    ~Config();
 
-  /**
-   * @brief Get a member of configuration and casts it as a boolean
-   */
-  bool getAsBool(std::string const &id);
+    /**
+     * @brief Checks if an id is a config member.
+     *
+     * @param id The id to check
+     * @return true The id is a member of the configuration
+     * @return false The id is not a member of the configuration
+     */
+    bool exists(std::string const &id);
 
-  /**
-   * @brief Get a member of configuration and casts it as an unsigned int
-   */
-  unsigned int getAsUInt(std::string const &id);
+    /**
+     * @brief Sets the value of a configuration member.
+     *
+     * @param id The id of the member.
+     * @param value The value to set.
+     */
+    void set(std::string const &id, std::string const &value);
 
-  /**
-   * @brief Logs the config
-   *
-   */
-  void logConfig();
+    /**
+     * @brief Gets the value of a configuration member.
+     *
+     * @param id The id of the member.
+     * @return std::string The value of the member. Empty string if the member
+     * is not found.
+     */
+    std::string get(std::string const &id);
 
-private:
-  /** Strores string config members*/
-  std::unordered_map<std::string, std::string> configMap;
+    /**
+     * @brief Get a member of configuration and casts it as a boolean
+     */
+    bool getAsBool(std::string const &id);
+
+    /**
+     * @brief Get a member of configuration and casts it as an unsigned int
+     */
+    unsigned int getAsUInt(std::string const &id);
+
+    /**
+     * @brief Logs the config
+     *
+     */
+    void logConfig();
+
+   private:
+    /** Strores string config members*/
+    std::unordered_map<std::string, std::string> configMap;
 };
 
 #endif
